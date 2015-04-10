0fea6b68d99786541576abdfdbac0f1bc8414c97
Szymon Jakubczak <szym@szym.net>
2015-04-10 16:43:13 -0700

    simple bash utility to just show files

---
+++ show.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Using openssl and curl send the image file to the display server.
+# This is a bit roundabout but this way we don't need to put the file in a place
+# from where the server could serve it as static.
+
+set -e
+shopt -s nocasematch
+
+TITLE=''
+WIN='null'
+
+
+function usage() {
+  cat >&2 <<EOF
+Usage: [-t title] [-w id] -i imagefile
+EOF
+}
+
+if [ "$#" -eq 0 ]; then
+  usage
+  exit 2
+fi
+
+
+while getopts "t:w:i:h" opt; do
+  case $opt in
+    t)
+      TITLE=$OPTARG
+      ;;
+    w)
+      WIN="\"win\": \"$OPTARG\","
+      ;;
+    i)
+      IMGFILE=$OPTARG
+      ;;
+    h)
+      usage
+      exit 2
+      ;;
+  esac
+done
+
+
+EXT=${IMGFILE##*.}
+case $EXT in
+  jpg) EXT="jpeg"
+    ;;
+  png|gif|bmp|webp|pdf)
+    ;;
+  *)
+    echo >&2 "Warning: unrecognized image format: $EXT"
+    ;;
+esac
+
+function compose() { 
+  cat <<EOF
+  { "command": "image", "title":"$TITLE", "src":"data:image/${EXT};base64,
+EOF
+  openssl enc -base64 -in $IMGFILE
+  echo '"}'
+}
+
+compose | curl -X POST -d @- http://localhost:8000/events
